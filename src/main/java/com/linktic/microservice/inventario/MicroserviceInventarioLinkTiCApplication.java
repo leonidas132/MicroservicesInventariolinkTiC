9ec9e817ac4327e617b72790725037ea93cbfa20
leonidas132 <luismrocha132@gmail.com>
2025-11-10 20:41:29 -0500

    TICKET-001: Se inicializa el repositorio con la estructura correspondiente para el microservicio MicroserviceProductosLinkTiC.

---
+++ src/main/java/com/linktic/microservice/inventario/MicroserviceInventarioLinkTiCApplication.java
@@ -0,0 +1,13 @@
+package com.linktic.microservice.inventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceInventarioLinkTiCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceInventarioLinkTiCApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/linktic/microservice/inventario/MicroserviceInventarioLinkTiCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linktic.microservice.inventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceInventarioLinkTiCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
